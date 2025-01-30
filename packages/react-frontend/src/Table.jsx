aea2c9714a49c9ffe43b36cedbb997837f66a336
Edgar Garcia <97008141+EdgarJGarcia@users.noreply.github.com>
2025-01-29 23:43:10 -0800

    showing id column

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -4,20 +4,23 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody 
-	  characterData={props.characterData}
-	  removeCharacter={props.removeCharacter}
-	/>
+        <TableBody
+          characterData={props.characterData}
+          removeCharacter={props.removeCharacter}
+        />
       </table>
     );
 }
+
 // src/Table.jsx
 function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Job</th>
+        <th>Actions</th>
       </tr>
     </thead>
   );
@@ -27,22 +30,20 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-      <td>{row.name}</td>
-      <td>{row.job}</td>
-      <td>
-         <button onClick={() => props.removeCharacter(index)}>
-          Delete
-         </button>
-      </td>
-    </tr>
+        <td>{row.id}</td>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
-   }
-  );
+  });
   return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
+    <tbody>
+      {rows}
+    </tbody>
+  );
 }
 
 export default Table;
