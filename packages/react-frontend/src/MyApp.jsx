0c2f45124a3d177f2994de3f4c1a07120419c157
Edgar Garcia <97008141+EdgarJGarcia@users.noreply.github.com>
2025-01-29 23:24:09 -0800

    id and error code, still trying to get delete to work on backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, { useState, useEffect } from "react";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
@@ -15,15 +15,59 @@ function MyApp() {
     </div>
   );
 
-function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+useEffect(() => {
+  fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => {
+      console.log(error);
     });
-    setCharacters(updated);
-  }
+}, []);
+
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(person)
+  });
+
+  return promise;
+}
+
+function removeOneCharacter(index) {
+  const id = characters[index].id;
+  const url = `http://localhost:8000/users/${id}`;
+  fetch(url, { method: 'DELETE' })
+    .then((response) => {
+      if (response.ok) {
+        const updated = characters.filter((character, i) => {
+          return i !== index;
+        });
+        setCharacters(updated);
+      } else {
+        console.error('Error deleting user:', response.status);
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting user:', error);
+    });
+}
+
 
 function updateList(person) {
-  setCharacters([...characters, person]);
+  postUser(person)
+    .then(response => response.json())
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function fetchUsers() {
+  const promise = fetch("http://localhost:8000/users");
+  return promise;
 }
 
 }
